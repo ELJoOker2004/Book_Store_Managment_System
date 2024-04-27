dad876dcdeb14f353e5081505d8e1e44a506caee
Tarek Ibrahim <tarek.ibr007@gmail.com>
2024-04-28 01:38:49 +0300

    data base created

--- main.py
+++ main.py
@@ -1,4 +1,43 @@
 import test
+import sqlite3
+
+
+def insertRowInTable(name, age):
+    db_path = "book_store.db"
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    #Insert rows into the table
+    cursor.execute("INSERT INTO my_table (name, age) VALUES (?, ?)", (name, age))
+
+    # Commit changes and close the connection
+    conn.commit()
+    conn.close()
+
+def printRowInTable(n):
+    db_path = "book_store.db"
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    # Insert rows into the table
+    # cursor.execute("INSERT INTO my_table (name, age) VALUES (?, ?)", ("Alice", 30))
+
+
+    cursor.execute(f"SELECT * FROM my_table WHERE name = ?", (n,))
+    row = cursor.fetchone()
+
+    if row:
+        print(f"ID: {row[0]}")
+        print(f"Name: {row[1]}")
+        print(f"Age: {row[2]}")
+    else:
+        print("No record found for Alice.")
+
+    # Commit changes and close the connection
+    conn.commit()
+    conn.close()
+
+
 #sample from korea 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -9,3 +48,6 @@ test.printing();
 if __name__ == '__main__':
     print_hi('PyCharm')
     print("Fuck Omar")
+    printRowInTable("Alice")
+    insertRowInTable("eljooker", 19)
+    printRowInTable("eljooker")
